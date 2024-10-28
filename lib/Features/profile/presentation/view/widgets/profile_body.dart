5117881da2bda0435eb194f76384e265317e7f98
Amr-3li <amrali51111@gmail.com>
2024-10-28 18:23:11 +0300

    create profile inormation class

--- lib/Features/profile/presentation/view/widgets/profile_body.dart
+++ lib/Features/profile/presentation/view/widgets/profile_body.dart
@@ -8,22 +8,40 @@ class ProfileBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Container(
-          decoration: const BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                  color: MyColors.shadowColor,
-                  blurRadius: 20,
-                  offset: Offset(0, 10))
-            ],
-            borderRadius: BorderRadius.all(Radius.circular(20)),
-          ),
-          child:
-          const Column(children: [
-            
-          ])
-          ),
+      child: Column(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                      color: MyColors.shadowColor,
+                      blurRadius: 20,
+                      offset: Offset(0, 10))
+                ],
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: const ProfileInformation()),
+        ],
+      ),
     );
+  }
+}
+
+class ProfileInformation extends StatelessWidget {
+  const ProfileInformation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(children: [
+      SizedBox(height: 20),
+      Text(
+        "email",
+        style: TextStyle(color: Colors.grey),
+      )
+      
+    ]);
   }
 }
