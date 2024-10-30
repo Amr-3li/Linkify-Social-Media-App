7fc7918364cdfdb57e12906dac1bbfff5b220706
Amr-3li <amrali51111@gmail.com>
2024-10-30 10:09:37 +0300

    featch user data in profile page by cubit

--- lib/Features/profile/presentation/view/widgets/profile_body.dart
+++ lib/Features/profile/presentation/view/widgets/profile_body.dart
@@ -1,5 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:true_gym/bloc/cubit/user_data/user_cubit.dart';
 import 'package:true_gym/consts.dart';
+import 'package:true_gym/data/models/user.dart';
 
 class ProfileBody extends StatelessWidget {
   const ProfileBody({super.key});
@@ -11,6 +15,7 @@ class ProfileBody extends StatelessWidget {
       child: Column(
         children: [
           Container(
+              width: double.infinity,
               decoration: const BoxDecoration(
                 color: Colors.white,
                 boxShadow: [
@@ -21,27 +26,55 @@ class ProfileBody extends StatelessWidget {
                 ],
                 borderRadius: BorderRadius.all(Radius.circular(20)),
               ),
-              child: const ProfileInformation()),
+              child: BlocProvider(
+                create: (context) => UserCubit()..getUserData(),
+                child: const ProfileInformation(),
+              )),
         ],
       ),
     );
   }
 }
 
-class ProfileInformation extends StatelessWidget {
+class ProfileInformation extends StatefulWidget {
   const ProfileInformation({
     super.key,
   });
 
   @override
+  State<ProfileInformation> createState() => _ProfileInformationState();
+}
+
+class _ProfileInformationState extends State<ProfileInformation> {
+  late UserModel user;
+  @override
+  @override
   Widget build(BuildContext context) {
-    return const Column(children: [
-      SizedBox(height: 20),
-      Text(
-        "email",
-        style: TextStyle(color: Colors.grey),
-      )
-      
-    ]);
+    return BlocBuilder<UserCubit, UserState>(
+      builder: (context, state) {
+        if (state is UserLoading) {
+          // Show a loading indicator while the data is being fetched
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is UserLoaded) {
+          // Show user data when loaded
+          final user = state.user;
+          return Column(
+            children: [
+              const SizedBox(height: 20),
+              Text("Email: ${user.email}"),
+              const SizedBox(height: 20),
+              Text("Name: ${user.name}"),
+              const SizedBox(height: 20),
+              Text("phone: ${user.phone}"),
+              // Add more user details here
+            ],
+          );
+        } else if (state is UserError) {
+          // Show an error message if something goes wrong
+          return Center(child: Text('Error: ${state.message}'));
+        }
+        return const SizedBox.shrink(); // Show nothing by default
+      },
+    );
   }
 }
