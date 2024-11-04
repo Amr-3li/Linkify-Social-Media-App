815603abbe54bbd921955fd77c84b78e81c2daec
Amr-3li <amrali51111@gmail.com>
2024-11-04 09:58:02 +0200

    finish ui of settings page

--- lib/Features/settings/presentation/view/widgets/settings_body.dart
+++ lib/Features/settings/presentation/view/widgets/settings_body.dart
@@ -7,15 +7,25 @@ class SettingsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        ListNameComponent(name: "GENERAL"),
-        SettingsListItem(
-            text: "language", icon: Icons.language, trailingText: "English"),
-        SettingsListItem(
-            text: "theme", icon: Icons.brightness_4, trailingText: "Light"),
-        SettingsListItem(text: "notifications", icon: Icons.notifications),
-      ],
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          ListNameComponent(name: "GENERAL"),
+          SettingsListItem(
+              text: "language", icon: Icons.language, trailingText: "English"),
+          SettingsListItem(
+              text: "theme", icon: Icons.brightness_4, trailingText: "Light"),
+          SettingsListItem(text: "notifications", icon: Icons.notifications),
+          ListNameComponent(name: "ACCOUNT"),
+          SettingsListItem(text: "edit profile", icon: Icons.person),
+          SettingsListItem(text: "change password", icon: Icons.password),
+          ListNameComponent(name: "HELP"),
+          SettingsListItem(text: "contact us", icon: Icons.contact_page),
+          SettingsListItem(text: "about us", icon: Icons.info),
+          ListNameComponent(name: "OTHERS"),
+          SettingsListItem(text: "logout", icon: Icons.logout),
+        ],
+      ),
     );
   }
 }
