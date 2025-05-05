a2043b1aadb94f7b19c6b5e5a923152aa7c8106f
Amr Ali <140944803+Amr-3li@users.noreply.github.com>
2025-05-05 10:39:13 +0300

    edit in function of publish post and get posts timeline

--- lib/Features/home/data/wep_serveice/get_posts_serv_impl.dart
+++ lib/Features/home/data/wep_serveice/get_posts_serv_impl.dart
@@ -3,20 +3,40 @@ import 'package:linkify/Features/home/data/Models/comment_model.dart';
 import 'package:linkify/Features/home/data/Models/lover_model.dart';
 import 'package:linkify/Features/home/data/wep_serveice/get_posts_serv.dart';
 import 'package:linkify/Features/home/data/Models/post_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GetPostsServImpl implements GetPostsServ {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  SharedPreferences? _prefs;
+  List<PostModel> glopalPosts = [];
+  int postsCount = 0;
+
+  DocumentSnapshot? lastPostDoc;
   @override
   Future<List<PostModel>> getMyTimelinePosts() async {
-    List<PostModel> posts = [];
-    await firestore
-        .collection('posts')
-        .orderBy('time', descending: true)
-        .get()
-        .then((value) {
-      posts = value.docs.map((e) => PostModel.fromJson(e.data())).toList();
-    });
-    return posts;
+    final currentUserId = await _getCurrentUserId();
+    Query query = firestore
+        .collection('users')
+        .doc(currentUserId)
+        .collection('timeline')
+        .limit(20);
+    if (lastPostDoc != null) {
+      query = query.startAfterDocument(lastPostDoc!);
+    }
+    final snapshot = await query.get();
+    if (snapshot.docs.isEmpty) return glopalPosts;
+    lastPostDoc = snapshot.docs.last;
+    final localPosts = snapshot.docs
+        .map((e) => PostModel.fromJson(e.data() as Map<String, dynamic>))
+        .toList();
+    glopalPosts.addAll(localPosts);
+    return glopalPosts;
+  }
+
+  Future<List<PostModel>> refreshTimeline() async {
+    glopalPosts.clear();
+    lastPostDoc = null;
+    return await getMyTimelinePosts();
   }
 
   @override
@@ -37,5 +57,33 @@ class GetPostsServImpl implements GetPostsServ {
       lovers = post.likes;
     });
     return lovers;
+  }
+
+  //=====================================================
+  // Future<List<String>> _getFriendIds() async {
+  //   final currentUserId = await _getCurrentUserId();
+
+  //   final querySnapshot = await firestore
+  //       .collection('friendRequests')
+  //       .where('status', isEqualTo: Constants.accepted)
+  //       .where(
+  //         Filter.or(
+  //           Filter('senderId', isEqualTo: currentUserId),
+  //           Filter('receiverId', isEqualTo: currentUserId),
+  //         ),
+  //       )
+  //       .get();
+
+  //   return querySnapshot.docs.map((doc) {
+  //     final data = doc.data();
+  //     return data['senderId'] == currentUserId
+  //         ? data['receiverId']
+  //         : data['senderId'];
+  //   }).toList() as List<String>;
+  // }
+
+  Future<String> _getCurrentUserId() async {
+    _prefs ??= await SharedPreferences.getInstance();
+    return _prefs!.getString('uid')!;
   }
 }
--- lib/Features/posts/data/services/post_serv_impl.dart
+++ lib/Features/posts/data/services/post_serv_impl.dart
@@ -1,7 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore;
+import 'package:cloud_firestore/cloud_firestore.dart'
+    show Filter, FirebaseFirestore;
 import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth, User;
 import 'package:linkify/Features/home/data/Models/post_model.dart';
 import 'package:linkify/Features/posts/data/services/post_serv.dart';
+import 'package:linkify/core/constants/constants.dart';
 
 class AddPostImpl implements AddPost {
   FirebaseAuth auth = FirebaseAuth.instance;
@@ -11,5 +13,36 @@ class AddPostImpl implements AddPost {
   @override
   Future<void> addPost(PostModel post) async {
     await firestore.collection('posts').doc(post.time).set(post.toJson());
+    List<String> friendIds = await _getFriendIds();
+    for (String id in friendIds) {
+      await firestore
+          .collection('users')
+          .doc(id)
+          .collection('timeline')
+          .doc(post.time)
+          .set(post.toJson());
+    }
+  }
+
+  Future<List<String>> _getFriendIds() async {
+    final currentUserId = user.uid;
+
+    final querySnapshot = await firestore
+        .collection('friendRequests')
+        .where('status', isEqualTo: Constants.accepted)
+        .where(
+          Filter.or(
+            Filter('senderId', isEqualTo: currentUserId),
+            Filter('receiverId', isEqualTo: currentUserId),
+          ),
+        )
+        .get();
+
+    return querySnapshot.docs.map((doc) {
+      final data = doc.data();
+      return data['senderId'] == currentUserId
+          ? data['receiverId']
+          : data['senderId'];
+    }).toList() as List<String>;
   }
 }
